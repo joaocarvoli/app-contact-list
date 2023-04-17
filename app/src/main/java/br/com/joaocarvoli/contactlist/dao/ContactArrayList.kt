76da83b0257cc109d76099b1a2617da4952db2db
João Victor <joaocarvoli@hotmail.com>
2023-04-17 16:31:18 -0300

    Creating dao package to operate the Contact class using other structures

---
+++ app/src/main/java/br/com/joaocarvoli/contactlist/dao/ContactArrayList.kt
@@ -0,0 +1,36 @@
+package br.com.joaocarvoli.contactlist.dao
+
+import br.com.joaocarvoli.contactlist.model.Contact
+
+class ContactArrayList : ContactDAO {
+
+    private var contactList : ArrayList<Contact> = ArrayList<Contact>()
+
+    override fun add(contact: Contact): Boolean {
+       contactList.add(contact)
+       return true
+    }
+
+    override fun remove(id: Number): Boolean {
+        contactList.forEach {contactInList ->
+            if(contactInList.getId() == id){
+                contactList.remove(contactInList)
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun removeAll(): Boolean {
+        return false
+    }
+
+    override fun get(id: Number): Contact {
+        return Contact(1,"", 1, "")
+    }
+
+    override fun getAll(): ArrayList<Contact> {
+        return arrayListOf()
+    }
+
+}
---
+++ app/src/main/java/br/com/joaocarvoli/contactlist/dao/ContactDAO.kt
@@ -0,0 +1,11 @@
+package br.com.joaocarvoli.contactlist.dao
+
+import br.com.joaocarvoli.contactlist.model.Contact
+
+interface ContactDAO {
+    fun add(contact: Contact) : Boolean
+    fun remove(id : Number) : Boolean
+    fun removeAll() : Boolean
+    fun get(id : Number) : Contact
+    fun getAll() : ArrayList<Contact>
+}
