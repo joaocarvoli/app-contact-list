4c0a5ffe36e169cf85860139597bcc3fc12eb3e8
João Victor <joaocarvoli@hotmail.com>
2023-04-17 16:31:49 -0300

    Creating add contact test button

--- app/src/main/java/br/com/joaocarvoli/contactlist/MainActivity.kt
+++ app/src/main/java/br/com/joaocarvoli/contactlist/MainActivity.kt
@@ -2,10 +2,15 @@ package br.com.joaocarvoli.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAddContact : Button = findViewById(R.id.btn_add)
+        btnAddContact.setOnClickListener {}
     }
 }
